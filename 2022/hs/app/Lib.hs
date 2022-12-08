8b2decbeded10583e4c560592e4cd621c865077c
Dawnflash <adamzahumensky@gmail.com>
2022-12-08 13:44:40 +0100

    Day 8

--- 2022/hs/app/Lib.hs
+++ 2022/hs/app/Lib.hs
@@ -9,3 +9,30 @@ splitWhen p s = case dropWhile p s of
 
 split :: Eq a => a -> [a] -> [[a]]
 split d = splitWhen (== d)
+
+-- 2D helpers
+
+type Point2D = (Int, Int)
+data Direction2D = DirUp | DirDown | DirLeft | DirRight
+
+point2DFromInt :: Int -> Int -> Point2D
+point2DFromInt w p = let (y, x) = divMod p w in (x, y)
+
+point2DToInt :: Int -> Point2D -> Int
+point2DToInt w (x, y) = w * y + x
+
+neigh2D :: Direction2D -> Point2D -> Point2D -> Maybe Point2D
+neigh2D d (w, h) (x, y) = neigh2D' d
+  where
+    neigh2D' DirUp
+      | y <= 0 = Nothing
+      | otherwise = Just (x, y - 1)
+    neigh2D' DirDown
+      | y + 1 >= h = Nothing
+      | otherwise = Just (x, y + 1)
+    neigh2D' DirLeft
+      | x <= 0 = Nothing
+      | otherwise = Just (x - 1, y)
+    neigh2D' DirRight
+      | x + 1 >= w = Nothing
+      | otherwise = Just (x + 1, y)
--- 2022/hs/app/S8.hs
+++ 2022/hs/app/S8.hs
@@ -1,6 +1,47 @@
 module S8 where
 
+import qualified Data.Vector as V
+import Data.Char ( ord )
+import Data.Maybe ( fromJust, isNothing )
+import Lib
+    ( Point2D, Direction2D(..), neigh2D, point2DFromInt, point2DToInt )
+
+type Forest = V.Vector Int
+
 main :: String -> IO ()
 main input = do
   let inputLines = lines input
-  print inputLines
+  let fsize@(width, height) = (length $ head inputLines, length inputLines)
+  let forest = parseInput width V.empty input
+  -- print $ isVisible forest fsize <$> [0..(length forest - 1)]
+  print $ sum $ fromEnum . or . directionalAnalysis forest fsize isVisibleDir . point2DFromInt width <$> [0..(length forest - 1)] -- part 1
+  print $ maximum $ fromEnum . product . directionalAnalysis forest fsize viewDistanceDir . point2DFromInt width <$> [0..(length forest - 1)] -- part 2
+
+parseInput :: Int -> Forest -> String -> Forest
+parseInput _ f [] = f
+parseInput w f ('\n':is) = parseInput w f is
+parseInput w f (i:is) = V.cons (ord i - ord '0') $ parseInput w f is
+
+directionalAnalysis :: Forest -> Point2D -> (Direction2D -> Forest -> Point2D -> Int -> Point2D -> a) -> Point2D -> [a]
+directionalAnalysis f dim@(w, _) fn p = [
+  fn DirUp f dim refHeight p,
+  fn DirDown f dim refHeight p,
+  fn DirLeft f dim refHeight p,
+  fn DirRight f dim refHeight p]
+  where
+    refHeight = f V.! point2DToInt w p
+
+isVisibleDir :: Direction2D -> Forest -> Point2D -> Int -> Point2D -> Bool
+isVisibleDir dir f dim@(w, _) refHeight p = case neigh2D dir dim p of
+  Just np -> refHeight > f V.! point2DToInt w np && isVisibleDir dir f dim refHeight np
+  Nothing -> True
+
+viewDistanceDir :: Direction2D -> Forest -> Point2D -> Int -> Point2D -> Int
+viewDistanceDir dir f dim@(w, _) refHeight p
+  | isNothing np = 0
+  | nh >= refHeight = 1
+  | otherwise = 1 + viewDistanceDir dir f dim refHeight jnp
+  where
+    np = neigh2D dir dim p
+    jnp = fromJust np
+    nh = f V.! point2DToInt w jnp
