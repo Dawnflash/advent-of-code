7c633ce02ddf918a60a49a3917002d87f4cd3bb3
Dawnflash <adamzahumensky@gmail.com>
2023-12-03 17:19:45 +0100

    2023 day 3

--- 2023/rust/src/t3.rs
+++ 2023/rust/src/t3.rs
@@ -1,35 +1,92 @@
+use std::{cmp::min, collections::HashMap};
+
+use itertools::Itertools;
+use regex::Regex;
+
 pub fn main(input: String) {
-    let sum: u64 = input
-        .lines()
-        .map(|line| {
-            let half = line.len() / 2;
-            ((bset(&line[..half]) & bset(&line[half..])) as f64)
-                .log2()
-                .round() as u64
+    let nre = Regex::new(r"\d+").unwrap();
+    let sre = Regex::new(r"[^\d.]").unwrap();
+    let mut gearmap: HashMap<usize, Vec<u64>> = HashMap::new();
+    let lines = input.lines().collect_vec();
+    let llen = lines[0].len();
+    let sum: u64 = lines
+        .iter()
+        .enumerate()
+        .map(|(nline, line)| {
+            let loffset = nline * llen;
+            nre.find_iter(line)
+                .map(|m| {
+                    let n: u64 = m.as_str().parse().unwrap();
+                    let prev = if m.start() > 0 { m.start() - 1 } else { 0 };
+                    let next = min(llen, m.end() + 1);
+                    let mut found = false;
+                    // check surrounding characters
+                    if m.start() > 0 {
+                        found |= process_adjacent(
+                            &mut gearmap,
+                            &sre,
+                            &line[prev..m.start()],
+                            loffset + prev,
+                            n,
+                        );
+                    }
+                    if m.end() < llen {
+                        found |= process_adjacent(
+                            &mut gearmap,
+                            &sre,
+                            &line[m.end()..next],
+                            loffset + m.end(),
+                            n,
+                        );
+                    }
+                    // check adjacent lines
+                    if nline > 0 {
+                        found |= process_adjacent(
+                            &mut gearmap,
+                            &sre,
+                            &lines[nline - 1][prev..next],
+                            loffset - llen + prev,
+                            n,
+                        );
+                    }
+                    if nline < lines.len() - 1 {
+                        found |= process_adjacent(
+                            &mut gearmap,
+                            &sre,
+                            &lines[nline + 1][prev..next],
+                            loffset + llen + prev,
+                            n,
+                        );
+                    }
+                    n * found as u64
+                })
+                .sum::<u64>()
         })
         .sum();
-    println!("{sum}");
-    // part 2
-    let sum: u64 = input
-        .lines()
-        .array_chunks()
-        .map(|[a, b, c]| ((bset(a) & bset(b) & bset(c)) as f64).log2().round() as u64)
-        .sum();
-    println!("{sum}");
+    println!("Part 1: {}", sum);
+    println!(
+        "Part 2: {}",
+        gearmap
+            .values()
+            .map(|v| if v.len() == 2 { v.iter().product() } else { 0 })
+            .sum::<u64>()
+    );
 }
 
-fn bset(s: &str) -> u64 {
-    s.as_bytes()
-        .iter()
-        .map(|c| 1 << prio(*c))
-        .reduce(|acc, p| acc | p)
-        .expect("forming bset failed")
-}
-
-fn prio(item: u8) -> u8 {
-    match item {
-        b'a'..=b'z' => item - b'a' + 1,
-        b'A'..=b'Z' => item - b'A' + 27,
-        _ => unreachable!("invalid item"),
+fn process_adjacent(
+    gearmap: &mut HashMap<usize, Vec<u64>>,
+    regex: &Regex,
+    data: &str,
+    offset: usize,
+    n: u64,
+) -> bool {
+    if !regex.is_match(data) {
+        return false;
     }
+    data.chars().enumerate().for_each(|(pos, c)| {
+        if c == '*' {
+            gearmap.entry(offset + pos).or_insert(Vec::new()).push(n);
+        }
+    });
+    return true;
 }
