814a7ff97dd4b4957c907a596d99ee3ee03609f7
Dawnflash <adamzahumensky@gmail.com>
2023-12-06 12:26:26 +0100

    2023 day 6

--- 2023/rust/src/t6.rs
+++ 2023/rust/src/t6.rs
@@ -1,10 +1,38 @@
 use itertools::Itertools;
 
 pub fn main(input: String) {
-    let b = input.as_bytes();
-    println!("{}\n{}", detect(b, 4), detect(b, 14));
-}
-
-fn detect(s: &[u8], sz: usize) -> usize {
-    sz + s.windows(sz).position(|w| w.iter().all_unique()).unwrap()
+    let (times, dists) = input
+        .lines()
+        .map(|l| {
+            l.split_once(":")
+                .unwrap()
+                .1
+                .split_whitespace()
+                .map(|s| s.parse::<u64>().unwrap())
+                .collect_vec()
+        })
+        .collect_tuple()
+        .unwrap();
+    let p1: usize = times
+        .iter()
+        .zip(dists.iter())
+        .map(|(&t, &d)| (1..t).filter(|&n| n * (t - n) > d).count())
+        .product();
+    println!("Part 1: {}", p1);
+    let (p2time, p2distance) = input
+        .lines()
+        .map(|l| {
+            l.split_once(":")
+                .unwrap()
+                .1
+                .replace(" ", "")
+                .parse::<u64>()
+                .unwrap()
+        })
+        .collect_tuple()
+        .unwrap();
+    let p2 = (1..p2time)
+        .filter(|&n| n * (p2time - n) > p2distance)
+        .count();
+    println!("Part 2: {}", p2);
 }
