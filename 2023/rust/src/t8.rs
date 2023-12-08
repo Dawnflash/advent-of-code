d64dea283e7570b1286c11667ee9e298d002abeb
Dawnflash <adamzahumensky@gmail.com>
2023-12-08 09:28:40 +0100

    2023 day 8

--- 2023/rust/src/t8.rs
+++ 2023/rust/src/t8.rs
@@ -1,62 +1,75 @@
-use aoc2023::*;
+use std::collections::HashMap;
+
 use itertools::Itertools;
-
-struct Forest {
-    data: Vec<Vec<u8>>,
-    dims: Point2D,
-}
-
-impl Forest {
-    fn new() -> Self {
-        Self {
-            data: vec![],
-            dims: Point2D::new(0, 0),
-        }
-    }
-}
+use num::integer::lcm;
 
 pub fn main(input: String) {
-    let mut forest: Forest = Forest::new();
-    for line in input.lines() {
-        forest
-            .data
-            .push(line.bytes().map(|b| b - b'0').collect_vec());
-    }
-    forest.dims.x = forest.data[0].len().try_into().unwrap();
-    forest.dims.y = forest.data.len().try_into().unwrap();
-    let results = (0..forest.dims.x)
-        .cartesian_product(0..forest.dims.y)
-        .map(|(x, y)| inspect(&forest, Point2D::new(x, y)))
-        .collect_vec();
-    println!("{}", results.iter().filter(|&b| b.0).count());
-    println!("{}", results.iter().map(|&r| r.1).max().unwrap());
-}
-
-fn inspect(forest: &Forest, p: Point2D) -> (bool, usize) {
-    let dirs = [
-        Direction2D::U,
-        Direction2D::R,
-        Direction2D::D,
-        Direction2D::L,
-    ];
-    let zero = Point2D::origin();
-    let ht: u8 = index_2d!(forest.data, p);
-    let mut vis = false;
-    let mut score = 1;
-    for dir in dirs {
-        let mut dvis = true;
-        let mut dscore = 0;
-        let mut np = p;
-        while let Some(nnp) = np.step_2d(zero, forest.dims, dir) {
-            np = nnp;
-            dscore += 1;
-            if index_2d!(forest.data, np) >= ht {
-                dvis = false;
-                break;
+    let (sdir, sgraph) = input.split_once("\n\n").unwrap();
+    let graph: HashMap<&str, (&str, &str)> = sgraph
+        .lines()
+        .map(|l: &str| {
+            let (node, edges) = l.split_once(" = ").unwrap();
+            let (l, r) = edges[1..edges.len() - 1].split_once(", ").unwrap();
+            (node, (l, r))
+        })
+        .collect();
+    let (p1, _) = sdir
+        .chars()
+        .cycle()
+        .fold_while((0, "AAA"), |(n, node), dir| {
+            if node == "ZZZ" {
+                return itertools::FoldWhile::Done((n, node));
             }
-        }
-        score *= dscore;
-        vis |= dvis;
-    }
-    (vis, score)
+            let (l, r) = graph[node];
+            let next = match dir {
+                'L' => l,
+                'R' => r,
+                _ => panic!("Invalid direction"),
+            };
+            itertools::FoldWhile::Continue((n + 1, next))
+        })
+        .into_inner();
+    println!("Part 1: {}", p1);
+
+    let p2keys = graph
+        .keys()
+        .filter(|&&k| k.ends_with("A"))
+        .cloned()
+        .collect_vec();
+    let mut p2loop_steps: HashMap<&str, u64> = p2keys.iter().map(|&k| (k, 0)).collect();
+    let p2 = p2keys
+        .iter()
+        .map(|&k| {
+            let res = sdir
+                .chars()
+                .cycle()
+                .fold_while((0, k), |(n, node), dir| {
+                    if node.ends_with("Z") {
+                        if p2loop_steps[k] == 0 {
+                            p2loop_steps.insert(k, n);
+                        } else {
+                            println!(
+                                "{}: {} % {} = {}",
+                                k,
+                                n,
+                                p2loop_steps[k],
+                                n % p2loop_steps[k]
+                            );
+                            return itertools::FoldWhile::Done((n - p2loop_steps[k], node));
+                        }
+                    }
+                    let (l, r) = graph[node];
+                    let next = match dir {
+                        'L' => l,
+                        'R' => r,
+                        _ => panic!("Invalid direction"),
+                    };
+                    itertools::FoldWhile::Continue((n + 1, next))
+                })
+                .into_inner()
+                .0;
+            res
+        })
+        .fold(1, |acc, n| lcm(acc, n));
+    println!("Part 2: {}", p2);
 }
