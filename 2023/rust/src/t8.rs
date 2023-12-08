1bf1764d4ab7c150cfd6bbcab1972da7d6ce8ef4
Dawnflash <adamzahumensky@gmail.com>
2023-12-08 11:07:30 +0100

    2023 day 8: shortening

--- 2023/rust/src/t8.rs
+++ 2023/rust/src/t8.rs
@@ -3,9 +3,11 @@ use std::collections::HashMap;
 use itertools::Itertools;
 use num::integer::lcm;
 
+type Graph<'a> = HashMap<&'a str, (&'a str, &'a str)>;
+
 pub fn main(input: String) {
-    let (sdir, sgraph) = input.split_once("\n\n").unwrap();
-    let graph: HashMap<&str, (&str, &str)> = sgraph
+    let (dirs, graph) = input.split_once("\n\n").unwrap();
+    let graph: Graph = graph
         .lines()
         .map(|l: &str| {
             let (node, edges) = l.split_once(" = ").unwrap();
@@ -13,11 +15,22 @@ pub fn main(input: String) {
             (node, (l, r))
         })
         .collect();
-    let (p1, _) = sdir
-        .chars()
+    let p1 = steps_until(&dirs, &graph, "AAA", |node| node == "ZZZ");
+    println!("Part 1: {}", p1);
+
+    let p2 = graph
+        .keys()
+        .filter(|&&k| k.ends_with("A"))
+        .map(|&k| steps_until(&dirs, &graph, k, |node| node.ends_with("Z")))
+        .fold(1, |acc, n| lcm(acc, n));
+    println!("Part 2: {}", p2);
+}
+
+fn steps_until(dirs: &str, graph: &Graph, start: &str, end: impl Fn(&str) -> bool) -> u64 {
+    dirs.chars()
         .cycle()
-        .fold_while((0, "AAA"), |(n, node), dir| {
-            if node == "ZZZ" {
+        .fold_while((0u64, start), |(n, node), dir| {
+            if end(node) {
                 return itertools::FoldWhile::Done((n, node));
             }
             let (l, r) = graph[node];
@@ -28,48 +41,6 @@ pub fn main(input: String) {
             };
             itertools::FoldWhile::Continue((n + 1, next))
         })
-        .into_inner();
-    println!("Part 1: {}", p1);
-
-    let p2keys = graph
-        .keys()
-        .filter(|&&k| k.ends_with("A"))
-        .cloned()
-        .collect_vec();
-    let mut p2loop_steps: HashMap<&str, u64> = p2keys.iter().map(|&k| (k, 0)).collect();
-    let p2 = p2keys
-        .iter()
-        .map(|&k| {
-            let res = sdir
-                .chars()
-                .cycle()
-                .fold_while((0, k), |(n, node), dir| {
-                    if node.ends_with("Z") {
-                        if p2loop_steps[k] == 0 {
-                            p2loop_steps.insert(k, n);
-                        } else {
-                            println!(
-                                "{}: {} % {} = {}",
-                                k,
-                                n,
-                                p2loop_steps[k],
-                                n % p2loop_steps[k]
-                            );
-                            return itertools::FoldWhile::Done((n - p2loop_steps[k], node));
-                        }
-                    }
-                    let (l, r) = graph[node];
-                    let next = match dir {
-                        'L' => l,
-                        'R' => r,
-                        _ => panic!("Invalid direction"),
-                    };
-                    itertools::FoldWhile::Continue((n + 1, next))
-                })
-                .into_inner()
-                .0;
-            res
-        })
-        .fold(1, |acc, n| lcm(acc, n));
-    println!("Part 2: {}", p2);
+        .into_inner()
+        .0
 }
