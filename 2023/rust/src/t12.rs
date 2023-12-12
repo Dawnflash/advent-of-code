1a903fb38fe07e9bae5fce377a071b77268b8cb1
Dawnflash <adamzahumensky@gmail.com>
2023-12-12 20:39:36 +0100

    2023 day 12

--- 2023/rust/src/t12.rs
+++ 2023/rust/src/t12.rs
@@ -1,82 +1,188 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 
-use aoc2023::*;
-use itertools::Itertools;
-
-struct Map {
-    start: Point2D,
-    end: Point2D,
-    dims: (usize, usize),
-    data: Vec<Vec<u8>>,
-}
-
-fn elevation(c: u8) -> u8 {
-    match c {
-        b'S' => elevation(b'a'),
-        b'E' => elevation(b'z'),
-        _ => c - b'a',
-    }
-}
-
-fn distances(map: &Map) -> Vec<Vec<usize>> {
-    let mut dists = vec![vec![usize::MAX; map.dims.0]; map.dims.1];
-    let mut cur = HashSet::new();
-    let mut buf = cur.clone();
-    let mut cur_dist = 0;
-    let bound = Point2D::new(map.dims.0 as i32, map.dims.1 as i32);
-
-    cur.insert(map.end); // starting point
-    while !cur.is_empty() {
-        for p in cur.drain() {
-            index_2d!(dists, p) = cur_dist;
-            buf.extend(p.adjacent_cross().iter().filter(|&&n| {
-                if !n.is_bounded(Point2D::origin(), bound) {
-                    return false;
-                }
-                let ph = index_2d!(map.data, p);
-                let nh = index_2d!(map.data, n);
-                index_2d!(dists, n) == usize::MAX && (ph < nh || ph - nh <= 1)
-            }));
-        }
-        cur.extend(buf.drain());
-        cur_dist += 1;
-    }
-    dists
-}
+type Memo = HashMap<(String, Vec<usize>), usize>;
 
 pub fn main(input: String) {
-    let lines = input.lines().collect_vec();
-    let mut start = Point2D::origin();
-    let mut end = Point2D::origin();
-    let dims = (lines[0].len(), lines.len());
-    let mut data = vec![vec![]; dims.1];
-    for (y, line) in input.lines().enumerate() {
-        if let Some(x) = line.find('S') {
-            start = Point2D::new(x as i32, y as i32);
+    let mut memo: Memo = HashMap::new();
+    let p1: usize = input
+        .lines()
+        .map(|line| {
+            let (layout, runs) = line.split_once(" ").unwrap();
+            let runs: Vec<usize> = runs.split(",").map(|c| c.parse().unwrap()).collect();
+            analyze(&layout, &runs, &mut memo)
+        })
+        .sum();
+    println!("Part 1: {}", p1);
+    let p2: usize = input
+        .lines()
+        .map(|line| {
+            let (_layout, _runs) = line.split_once(" ").unwrap();
+            let _runs: Vec<usize> = _runs.split(",").map(|c| c.parse().unwrap()).collect();
+            let runs = (0..5).fold(Vec::new(), |mut acc, _| {
+                acc.extend(_runs.iter());
+                acc
+            });
+            let layout = [_layout, _layout, _layout, _layout, _layout].join("?");
+            analyze(&layout, &runs, &mut memo)
+        })
+        .sum();
+    println!("Part 2: {}", p2);
+    println!("Cache size: {}kB", memo.len() * usize::BITS as usize / 8192);
+}
+
+fn analyze(layout: &str, runs: &Vec<usize>, memo: &mut Memo) -> usize {
+    let run_sum = runs.iter().sum::<usize>();
+    let slots = layout.chars().filter(|c| *c == '?').count();
+    let pending = run_sum - layout.chars().filter(|c| *c == '#').count();
+    let res = analyze_step(
+        layout.to_string(),
+        runs.clone(),
+        false,
+        slots,
+        pending,
+        memo,
+    );
+    memo.insert((layout.to_string(), runs.clone()), res);
+    res
+}
+
+// DFS
+fn analyze_step(
+    layout: String,
+    runs: Vec<usize>,
+    in_run: bool,
+    slots: usize,
+    pending: usize,
+    memo: &mut Memo,
+) -> usize {
+    if !in_run {
+        if let Some(&res) = memo.get(&(layout.to_string(), runs.clone())) {
+            return res;
         }
-        if let Some(x) = line.find('E') {
-            end = Point2D::new(x as i32, y as i32);
-        }
-        data[y].extend(line.bytes().map(elevation));
     }
-    let map = Map {
-        start: start,
-        end: end,
-        dims: dims,
-        data: data,
+    let cur = layout.chars().next();
+    // deterministic checks
+    if pending == 0 && cur.is_some() && !in_run {
+        let full = layout.replace("?", ".");
+        return if analyze_det(&full, &runs) { 1 } else { 0 };
+    }
+    if slots == pending && cur.is_some() && !in_run {
+        let full = layout.replace("?", "#");
+        return if analyze_det(&full, &runs) { 1 } else { 0 };
+    }
+    let runs_copy = runs.clone();
+    let res = match (cur, runs.get(0)) {
+        (None, None) => 1,
+        (None, Some(_)) => {
+            // # ending
+            if runs.iter().all(|&r| r == 0) {
+                1
+            } else {
+                0
+            }
+        }
+        (Some(_), None) => {
+            // . tail
+            if layout.chars().all(|c| c != '#') {
+                1
+            } else {
+                0
+            }
+        }
+        (Some(c), Some(&r)) => match c {
+            '.' => {
+                if in_run {
+                    if r != 0 {
+                        0
+                    } else {
+                        analyze_step(
+                            layout.chars().skip(1).collect(),
+                            runs.iter().skip(1).cloned().collect(),
+                            false,
+                            slots,
+                            pending,
+                            memo,
+                        )
+                    }
+                } else {
+                    analyze_step(
+                        layout.chars().skip(1).collect(),
+                        runs,
+                        false,
+                        slots,
+                        pending,
+                        memo,
+                    )
+                }
+            }
+            '#' => {
+                if r == 0 {
+                    0
+                } else {
+                    let mut runs = runs.clone();
+                    runs[0] -= 1;
+                    analyze_step(
+                        layout.chars().skip(1).collect(),
+                        runs,
+                        true,
+                        slots,
+                        pending,
+                        memo,
+                    )
+                }
+            }
+            '?' => {
+                if pending == slots {
+                    analyze_step(
+                        String::from('#') + &layout[1..],
+                        runs,
+                        in_run,
+                        slots - 1,
+                        pending - 1,
+                        memo,
+                    )
+                } else if pending == 0 {
+                    analyze_step(
+                        String::from('.') + &layout[1..],
+                        runs.clone(),
+                        in_run,
+                        slots - 1,
+                        pending,
+                        memo,
+                    )
+                } else {
+                    analyze_step(
+                        String::from('.') + &layout[1..],
+                        runs.clone(),
+                        in_run,
+                        slots - 1,
+                        pending,
+                        memo,
+                    ) + analyze_step(
+                        String::from('#') + &layout[1..],
+                        runs,
+                        in_run,
+                        slots - 1,
+                        pending - 1,
+                        memo,
+                    )
+                }
+            }
+            _ => panic!("unexpected char"),
+        },
     };
-    let dists = distances(&map);
+    if !in_run {
+        memo.insert((layout, runs_copy), res);
+    }
+    res
+}
 
-    println!("{}", index_2d!(dists, map.start)); // part 1
-
-    let closest_start = map
-        .data
-        .iter()
-        .flatten()
-        .zip(dists.iter().flatten())
-        .filter_map(|(&h, &d)| if h == 0 { Some(d) } else { None })
-        .min()
-        .unwrap();
-
-    println!("{}", closest_start); // part 2
+// fast deterministic check
+fn analyze_det(layout: &str, runs: &Vec<usize>) -> bool {
+    let res = layout
+        .split(".")
+        .filter(|s| !s.is_empty())
+        .map(|s| s.len())
+        .eq(runs.iter().filter(|&&v| v != 0).copied());
+    res
 }
