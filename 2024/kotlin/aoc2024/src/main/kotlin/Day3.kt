4488c5b399c1069d2f48dc0e3d47270ef2b5406a
Dawnflash <adam.zahumensky@productboard.com>
2024-12-03 12:58:55 +0100

    2024 day 3 polish

--- 2024/kotlin/aoc2024/src/main/kotlin/Day3.kt
+++ 2024/kotlin/aoc2024/src/main/kotlin/Day3.kt
@@ -4,12 +4,12 @@ class Day3 : Day() {
     override val sampleChecks = "161" to "48"
     override val checks = "173529487" to "99532691"
 
-    override fun solution1(input: List<String>): String {
-        val regex = Regex("""mul\((\d+),(\d+)\)""")
-        return regex.findAll(input.joinToString("")).sumOf { it.groupValues[1].toInt() * it.groupValues[2].toInt() }.toString()
-    }
-    override fun solution2(input: List<String>): String {
-        val dropRegex = Regex("""don't\(\).*?do\(\)""")
-        return solution1(input.joinToString("").replace(dropRegex, "").lines())
-    }
+    override fun solution1(input: List<String>) = solution(input.joinToString(""))
+    override fun solution2(input: List<String>) = solution(
+        input.joinToString("")
+            .replace(Regex("""don't\(\).*?do\(\)"""), "")
+    )
+
+    private fun solution(input: String) = Regex("""mul\((\d+),(\d+)\)""").findAll(input)
+        .sumOf { it.groupValues[1].toInt() * it.groupValues[2].toInt() }.toString()
 }
