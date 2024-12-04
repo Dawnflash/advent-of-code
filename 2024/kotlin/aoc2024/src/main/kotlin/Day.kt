91d2a33a71a0bbab59966e8e42e207804eda05e5
Dawnflash <adam.zahumensky@productboard.com>
2024-12-04 16:39:38 +0100

    add interval checks and not checks

--- 2024/kotlin/aoc2024/src/main/kotlin/Day.kt
+++ 2024/kotlin/aoc2024/src/main/kotlin/Day.kt
@@ -7,35 +7,50 @@ import kotlin.time.measureTimedValue
 open class Day {
     open val sampleChecks = "" to ""
     open val checks = "" to ""
+    open val notChecks: Pair<Array<String>, Array<String>> = arrayOf<String>() to arrayOf()
+    open val intervalChecks: Pair<Pair<String, String>, Pair<String, String>> = ("" to "") to ("" to "")
+
     open fun solution1(input: List<String>) = "???"
     open fun solution2(input: List<String>) = "???"
     private fun solvePart(part: Int, inputFile: File, sampleFile: File) {
-        val sampleCheck = if (part == 1) sampleChecks.first else sampleChecks.second
-        val check = if (part == 1) checks.first else checks.second
-        println("--${part}--")
+        val sampleCheck = sampleChecks.toList()[part - 1]
+        val check = checks.toList()[part - 1]
+        val notCheck = notChecks.toList()[part - 1]
+        val intervalCheck = intervalChecks.toList()[part - 1]
+        val (intMin, intMax) = intervalCheck.toList().map { it.toBigIntegerOrNull() }
+
+        println("--$part--")
         if (sampleFile.exists()) {
             val input = sampleFile.readText().lines().dropLastWhile { it.isEmpty() }
             val result = measureTimedValue { if (part == 1) solution1(input) else solution2(input) }
             val test = when (sampleCheck) {
                 "" -> "DONE"
                 result.value -> "PASS"
-                else -> "FAIL (${sampleCheck})"
+                else -> "FAIL ($sampleCheck)"
             }
-            println("Sample result [${test} in ${result.duration}]: ${result.value}")
+            println("Sample result [$test in ${result.duration}]: ${result.value}")
         }
         if (inputFile.exists()) {
             val input = inputFile.readText().lines().dropLastWhile { it.isEmpty() }
             val result = measureTimedValue { if (part == 1) solution1(input) else solution2(input) }
+            val resultNum = result.value.toBigIntegerOrNull()
             val test = when (check) {
-                "" -> "DONE"
+                "" -> when {
+                    notCheck.contains(result.value) -> "FAIL (!= ${result.value})"
+                    intMin != null && intMin >= resultNum -> "FAIL (> $intMin)"
+                    intMax != null && intMax <= resultNum -> "FAIL (< $intMax)"
+                    else -> "DONE"
+                }
+
                 result.value -> "PASS"
-                else -> "FAIL (${check})"
+                else -> "FAIL (== $check)"
             }
-            println("Result [${test} in ${result.duration}]: ${result.value}")
+            println("Result [$test in ${result.duration}]: ${result.value}")
         } else {
             println("! Main input file not found")
         }
     }
+
     fun solve() {
         val name = this::class.simpleName
         val inputFile = File(ClassLoader.getSystemResource("${name}.txt")?.file ?: "")
