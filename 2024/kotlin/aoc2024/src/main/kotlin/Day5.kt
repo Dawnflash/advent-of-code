cd1b978d9cdc9d939f7279b2dda8fe20c3dfaa81
Dawnflash <adam.zahumensky@productboard.com>
2024-12-05 15:37:05 +0100

    2024 day 5

--- 2024/kotlin/aoc2024/src/main/kotlin/Day5.kt
+++ 2024/kotlin/aoc2024/src/main/kotlin/Day5.kt
@@ -1,13 +1,39 @@
 package cz.dawnflash.aoc2024
 
 class Day5 : Day() {
-    override val sampleChecks = "" to ""
-    override val checks = "" to ""
+    override val sampleChecks = "143" to "123"
+    override val checks = "4959" to "4655"
+
+    private fun parse(input: List<String>): Pair<List<List<Int>>, List<List<Int>>> {
+        val splitter = input.indexOf("")
+        val rules = input.slice(0..<splitter).map { it.split('|').map(String::toInt) }
+        val updates = input.slice(splitter + 1..<input.size).map { it.split(',').map(String::toInt) }
+        return rules to updates
+    }
+
+    private fun isValid(rule: List<Int>, job: List<Int>): Boolean {
+        val indices = rule.map { job.indexOf(it) }
+        return indices.any { it == -1 } || indices[0] < indices[1]
+    }
+
+    private fun fix(rules: List<List<Int>>, job: List<Int>): List<Int> {
+        val applyingRules = rules.filter { rule -> job.any { rule[0] == it || rule[1] == it } }
+        return job.sortedWith { a, b ->
+            when {
+                applyingRules.any { it[0] == a && it[1] == b } -> -1
+                applyingRules.any { it[1] == a && it[0] == b } -> 1
+                else -> 0
+            }
+        }
+    }
 
     override fun solution1(input: List<String>): String {
-        return super.solution1(input)
+        val (rules, updates) = parse(input)
+        return updates.filter { rules.all { rule -> isValid(rule, it) } }.sumOf { it[it.size / 2] }.toString()
     }
+
     override fun solution2(input: List<String>): String {
-        return super.solution2(input)
+        val (rules, updates) = parse(input)
+        return updates.filter { rules.any { rule -> !isValid(rule, it) } }.sumOf { fix(rules, it)[it.size / 2] }.toString()
     }
 }
