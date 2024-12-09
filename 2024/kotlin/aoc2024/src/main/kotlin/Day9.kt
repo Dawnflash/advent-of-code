e2fed79da432ff5694842b306d3606fe5c2f372c
Dawnflash <adam.zahumensky@productboard.com>
2024-12-09 15:03:39 +0100

    day 9 improvs

--- 2024/kotlin/aoc2024/src/main/kotlin/Day9.kt
+++ 2024/kotlin/aoc2024/src/main/kotlin/Day9.kt
@@ -5,29 +5,32 @@ class Day9 : Day() {
     override val checks = "6461289671426" to "6488291456470"
 
     data class Block(var size: Int, var offset: Int)
+
     private fun checksum(disk: Array<Int?>): ULong = disk.foldIndexed(0UL) { index, acc, i ->
         acc + index.toULong() * (i ?: 0).toULong()
     }
+
     private fun checksum(files: List<Block>) = files.foldIndexed(0UL) { index, acc, i ->
-        acc + index.toULong() * (i.offset.toULong()..<(i.offset+i.size).toULong()).sum()
+        acc + index.toULong() * (i.offset.toULong()..<(i.offset + i.size).toULong()).sum()
     }
+
     private fun defragment(disk: Array<Int?>) {
         var l = 0
         var r = disk.size - 1
         while (l < r) {
-            if (disk[l] != null) {
-                l += 1
-                continue
+            when {
+                disk[l] != null -> l++
+                disk[r] == null -> r--
+                else -> {
+                    disk[l] = disk[r]
+                    disk[r] = null
+                    l++
+                    r--
+                }
             }
-            if (disk[r] == null) {
-                r -= 1
-                continue
-            }
-            disk[l] = disk[r].also { disk[r] = disk[l] }
-            l -= 1
-            r -= 1
         }
     }
+
     // this clobbers the voids (it doesn't create new voids at the end) but who cares
     private fun defragment(files: List<Block>, voids: List<Block>) {
         for (f in files.reversed()) {
@@ -41,9 +44,9 @@ class Day9 : Day() {
 
     override fun solution1(input: List<String>): String {
         val disk = input[0].flatMapIndexed { i, c ->
-            val cc = c.code - '0'.code; when {
-            i % 2 == 0 -> List(cc) { i / 2 }
-            else -> List(cc) { null }
+            val size = c.digitToInt(); when {
+            i % 2 == 0 -> List(size) { i / 2 }
+            else -> List(size) { null }
         }
         }.toTypedArray()
         defragment(disk)
@@ -53,11 +56,9 @@ class Day9 : Day() {
 
     override fun solution2(input: List<String>): String {
         var offset = 0
-        val blocks: List<Block> = input[0].indices.map {
-            val cc = (input[0][it].code - '0'.code)
-            val b = Block(cc, offset)
-            offset += cc
-            b
+        val blocks: List<Block> = input[0].map {
+            val size = it.digitToInt()
+            Block(size, offset).also { offset += size }
         }
         val files = blocks.filterIndexed { i, _ -> i % 2 == 0 }
         val voids = blocks.filterIndexed { i, _ -> i % 2 != 0 }
