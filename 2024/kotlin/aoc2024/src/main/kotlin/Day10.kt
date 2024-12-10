966af6dbc95c433c6aa27715b08c0e3d9fc42c22
Dawnflash <adam.zahumensky@productboard.com>
2024-12-10 19:47:42 +0100

    2024 day 10

--- 2024/kotlin/aoc2024/src/main/kotlin/Day10.kt
+++ 2024/kotlin/aoc2024/src/main/kotlin/Day10.kt
@@ -1,13 +1,30 @@
 package cz.dawnflash.aoc2024
 
+import cz.dawnflash.aoc2024.util.Point
+import cz.dawnflash.aoc2024.util.Map2D
+
 class Day10 : Day() {
-    override val sampleChecks = "" to ""
-    override val checks = "" to ""
+    override val sampleChecks = "36" to "81"
+    override val checks = "587" to "1340"
+
+    private fun numPaths(map: Map2D<Int>, pos: Point, cur: Int = 0): Int = when (cur) {
+        9 -> 1
+        else -> map.neighbors4(pos).filter { map.at(it) == cur + 1 }.sumOf { numPaths(map, it, cur + 1) }
+    }
+
+    private fun reachableEnds(map: Map2D<Int>, pos: Point, cur: Int = 0): Set<Point> = when (cur) {
+        9 -> setOf(pos)
+        else -> map.neighbors4(pos).filter { map.at(it) == cur + 1 }.map { reachableEnds(map, it, cur + 1) }
+            .fold(setOf()) { acc, e -> acc.union(e) }
+    }
 
     override fun solution1(input: List<String>): String {
-        return super.solution1(input)
+        val map = Map2D(input.map { line -> line.map { it.digitToInt() } })
+        return map.findAll { it == 0 }.sumOf { reachableEnds(map, it).size }.toString()
     }
+
     override fun solution2(input: List<String>): String {
-        return super.solution2(input)
+        val map = Map2D(input.map { line -> line.map { it.digitToInt() } })
+        return map.findAll { it == 0 }.sumOf { numPaths(map, it) }.toString()
     }
 }
--- 2024/kotlin/aoc2024/src/main/kotlin/util/Navigation.kt
+++ 2024/kotlin/aoc2024/src/main/kotlin/util/Navigation.kt
@@ -1,6 +1,7 @@
 package cz.dawnflash.aoc2024.util
 
 typealias Point = Pair<Int, Int>
+
 enum class Direction(val offset: Point) {
     N(0 to -1),
     NE(1 to -1),
@@ -22,8 +23,33 @@ enum class Direction(val offset: Point) {
         NW -> NE
     }
 
+    companion object {
+        val cardinals = listOf(Direction.N, Direction.E, Direction.S, Direction.W)
+        val diagonals = listOf(Direction.NE, Direction.SE, Direction.SW, Direction.NW)
+    }
+
     fun turnAround() = turnRight().turnRight()
     fun turnLeft() = turnRight().turnRight().turnRight()
+}
+
+data class Map2D<T>(val data: List<List<T>>) {
+    val h = data.size
+    val w = if (h == 0) 0 else data[0].size
+
+    fun at(p: Point): T = data[p.second][p.first]
+    fun atSafe(p: Point): T? = at(p).takeIf { isInside(p) }
+    fun isInside(p: Point) = p.first in 0..<w && p.second in 0..<h
+    fun dims(): Point = w to h
+    fun isValid(): Boolean = data.all { it.size == w }
+    fun findAll(predicate: (T) -> Boolean): List<Point> = data.flatMapIndexed { y, row ->
+        row.mapIndexedNotNull { x, i -> (x to y).takeIf { predicate(i) } }
+    }
+
+    fun neighbors(p: Point, dirs: List<Direction>): List<Point> = dirs.mapNotNull { dir ->
+        p.step(dir).takeIf { isInside(it) }
+    }
+    fun neighbors4(p: Point) = neighbors(p, Direction.cardinals)
+    fun neighbors8(p: Point) = neighbors(p, Direction.entries)
 }
 
 operator fun Point.plus(o: Point) = this.first + o.first to this.second + o.second
