4a14349988fe0cd30f92b7c781774ba4e6973fe3
Dawnflash <adam.zahumensky@productboard.com>
2024-12-19 08:00:51 +0100

    2024 day 19

--- 2024/kotlin/aoc2024/src/main/kotlin/Day19.kt
+++ 2024/kotlin/aoc2024/src/main/kotlin/Day19.kt
@@ -1,13 +1,29 @@
 package cz.dawnflash.aoc2024
 
 class Day19 : Day() {
-    override val sampleChecks = "" to ""
-    override val checks = "" to ""
+    override val sampleChecks = "6" to "16"
+    override val checks = "260" to "639963796864990"
+
+    private fun possible(pattern: String, towels: Set<String>, memo: HashMap<String, Long> = hashMapOf()): Long {
+        if (pattern.isEmpty()) return 1
+        return memo.getOrPut(pattern) {
+            pattern.indices.sumOf { i ->
+                if (towels.contains(pattern.substring(0, i + 1))) {
+                    possible(pattern.substring(i + 1, pattern.length), towels, memo)
+                } else 0
+            }
+        }
+    }
 
     override fun solution1(input: List<String>): String {
-        return super.solution1(input)
+        val towels = input[0].split(", ").toSet()
+        val desired = input.drop(2)
+        return desired.count { possible(it, towels) > 0 }.toString()
     }
+
     override fun solution2(input: List<String>): String {
-        return super.solution2(input)
+        val towels = input[0].split(", ").toSet()
+        val desired = input.drop(2)
+        return desired.sumOf { possible(it, towels) }.toString()
     }
 }
